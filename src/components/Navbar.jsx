20dcf505ad8e73a338fc79359da83a578fd4ba76
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-04-28 17:08:32 +0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,21 @@ const Navbar = () => {
           "p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sm:hidden",
           toggle ? "flex" : "hidden"
         )}
-      ></div>
+      >
+        <ul className="flex flex-col justify-end items-center flex-1">
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className={clsx(
+                "font-normal cursor-pointer text-[14px] text-white tracking-[2px] uppercase hover:text-secondary transition-all duration-150",
+                index === navLinks.length - 1 ? "mb-0" : "mb-4"
+              )}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
